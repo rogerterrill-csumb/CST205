891fdf217eb474bb365ca101f54c3f56b0dc6249
Roger Terrill <rchicasterill@gmail.com>
2019-03-14 12:39:00 -0700

    Lab 6 Problem 2 artify finished

--- CST205/lab6.py
+++ CST205/lab6.py
@@ -82,6 +82,47 @@ def sepia():
         setBlue(px, blue * 0.93)
   show(pic)
   return pic
+  
+# Problem 2
+def Artify():
+  pic = get_pic()
+  width = getWidth(pic)
+  height = getHeight(pic)
+  for x in range(0, width):
+    for y in range(0, height):
+      px = getPixel(pic, x, y)
+      red = getRed(px)
+      if( red < 64):
+        setRed(px, 31)
+      elif( red > 63 and red < 128):
+        setRed(px, 95)
+      elif( red > 127 and red < 192):
+        setRed(px, 159)
+      elif( red > 192 and red < 256):
+        setRed(px, 223)
+        
+      green = getGreen(px)
+      if( green < 64):
+        setGreen(px, 31)
+      elif( green > 63 and green < 128):
+        setGreen(px, 95)
+      elif( green > 127 and green < 192):
+        setGreen(px, 159)
+      elif( green > 192 and green < 256):
+        setGreen(px, 223)
+        
+      blue = getBlue(px)
+      if( blue < 64):
+        setBlue(px, 31)
+      elif( blue > 63 and blue < 128):
+        setBlue(px, 95)
+      elif( blue > 127 and blue < 192):
+        setBlue(px, 159)
+      elif( blue > 192 and blue < 256):
+        setBlue(px, 223)
+
+  show(pic)
+  return pic
     
     
  
