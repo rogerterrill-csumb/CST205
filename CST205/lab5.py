658e26ea8b533fa29d563f7ad8d7d2fa2141d9b9
RogerDodger <lpaddikt@gmail.com>
2019-03-13 19:03:30 -0700

    Added list get_pic()

--- CST205/lab5.py
+++ CST205/lab5.py
@@ -1,5 +1,66 @@
 def get_pic():
   return makePicture(pickAFile())
+  
+# Previous Functions
+def simpleCopy(pic):
+  width = getWidth(pic)
+  height = getHeight(pic)
+  mypic = makeEmptyPicture(width, height)
+  for x in range (0, getWidth(mypic)):
+    for y in range (0, getHeight(mypic)):
+      p = getPixel(pic, x, y)
+      red = getRed(p)
+      green = getGreen(p)
+      blue = getBlue(p)
+      new_pic = getPixel(mypic, x, y)
+      setRed(new_pic, red)
+      setGreen(new_pic, green)
+      setBlue(new_pic, blue)
+  show(mypic)
+  writePictureTo(mypic, "C://Users//roger.terrill//Documents//CST205//Photos/simple_copy.jpeg")
+  return mypic
+  
+  
+# Problem 3
+def rotatePic():
+  pic = get_pic()
+  width = getWidth(pic)
+  height = getHeight(pic)
+  mypic = makeEmptyPicture(height, width)
+  for x in range (0, width):
+    for y in range (0, height):
+      p = getPixel(pic, x, y)
+      red = getRed(p)
+      green = getGreen(p)
+      blue = getBlue(p)
+      new_pic = getPixel(mypic, y , width-x-1)
+      setRed(new_pic, red)
+      setGreen(new_pic, green)
+      setBlue(new_pic, blue)
+  show(mypic)
+  writePictureTo(mypic, "C://Users//roger.terrill//Documents//CST205//Photos/rotate_pic.jpeg")
+  return mypic
+  
+  
+# Problem 4
+def shrink():
+  pic = get_pic()
+  width = getWidth(pic)
+  height = getHeight(pic)
+  mypic = makeEmptyPicture(width/2, height/2)
+  for x in range (0, width, 2):
+    for y in range (0, height, 2):
+      p = getPixel(pic, x, y)
+      red = getRed(p)
+      green = getGreen(p)
+      blue = getBlue(p)
+      new_pic = getPixel(mypic, x/2, y/2)
+      setRed(new_pic, red)
+      setGreen(new_pic, green)
+      setBlue(new_pic, blue)
+  show(mypic)
+  writePictureTo(mypic, "C://Users//roger.terrill//Documents//CST205//Photos/shrink.jpeg")
+  return mypic
 
 # Warm up
 def copy_with_border():
@@ -42,18 +103,23 @@ def pyCopy(source, target, targetX, targetY):
   
 # Problem #2
 def makeCollage():
-  width = getWidth(source)
-  height = getHeight(source)
-  for x in range (0, width):
-    for y in range (0, height):
-      p = getPixel(source, x, y)
-      red = getRed(p)
-      green = getGreen(p)
-      blue = getBlue(p)
-      new_pic = getPixel(target, x+targetX, y+targetY)
-      setRed(new_pic, red)
-      setGreen(new_pic, green)
-      setBlue(new_pic, blue)
-  show(target)
-  writePictureTo(target, "C://Users//roger.terrill//Documents//CST205//Photos/pyCopy.jpeg")
-  return target
+  pic_list = []
+  for i in range(2):
+    pic_list.append(get_pic())
+    
+  print pic_list
+  print pic_list[1]
+  #width = getWidth(source)
+  #height = getHeight(source)
+  #for x in range (0, width):
+  #  for y in range (0, height):
+  #    p = getPixel(source, x, y)
+   #   red = getRed(p)
+    #  green = getGreen(p)
+     # blue = getBlue(p)
+      #new_pic = getPixel(target, x+targetX, y+targetY)
+      #setRed(new_pic, red)
+      #setGreen(new_pic, green)
+      #setBlue(new_pic, blue)
+  #show(target)
+  #writePictureTo(target, "C://Users//roger.terrill//Documents//CST205//Photos/pyCopy.jpeg")
