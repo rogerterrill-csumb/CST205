f062006b60bce834caec7e2fca74f534beed2244
Roger Dodger <lpaddikt@gmail.com>
2019-03-19 23:02:54 -0700

    Warholify fixing reference copy

--- CST205/warholify.py
+++ CST205/warholify.py
@@ -1,3 +1,5 @@
+import copy
+
 def get_pic():
     file = pickAFile()
     pic = makePicture(file)
@@ -21,40 +23,41 @@ def pyCopy(source, target, targetX, targetY):
 
 def warholify():
     pic = get_pic()
+    pic_list = []
+    
+    for i in range(4):
+      pic_list.append(copy.copy(pic))
     
     pic_width = getWidth(pic)
     pic_height = getHeight(pic)
     
-    red = pic
-    green = pic
-    blue = pic
-    pink = pic
     
     target = makeEmptyPicture(pic_width*2, pic_height*2)
-    pixels = getPixels(red)
+    
+    pixels = getPixels(pic_list[0])
     for p in pixels:
         r = getRed(p)
         setRed(p, r * 50)
-    pyCopy(red, target, 0,0)
+    pyCopy(pic_list[0], target, 0,0)
 
-    
-    pixels = getPixels(green)
+    pixels = getPixels(pic_list[1])
     for p in pixels:
         r = getGreen(p)
         setGreen(p, r * 30)
-    pyCopy(green, target, pic_width-1,0)
+    pyCopy(pic_list[1], target, pic_width-1,0)
  
-    pixels = getPixels(blue)
+    pixels = getPixels(pic_list[2])
     for p in pixels:
         r = getBlue(p)
         setBlue(p, r * 50)
-    pyCopy(blue, target, 0, pic_height-1)
+    pyCopy(pic_list[2], target, 0, pic_height-1)
 
     
-    pixels = getPixels(pink)
+    pixels = getPixels(pic_list[3])
     for p in pixels:
         r = getGreen(p)
         setGreen(p, r - 100)
-    pyCopy(blue, target, pic_width-1,pic_height-1)
+    pyCopy(pic_list[3], target, pic_width-1,pic_height-1)
+    
     
     show(target)
