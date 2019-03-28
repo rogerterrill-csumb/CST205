1c749223109f0eda1dc6c7eb3b37a5b8252e6857
Roger Dodger <lpaddikt@gmail.com>
2019-03-27 21:12:04 -0700

    maxVolume, goToEleven

--- CST205/lab8.py
+++ CST205/lab8.py
@@ -1,3 +1,7 @@
+def get_sound():
+  return makeSound(pickAFile())
+
+
 def increaseVolume(sound):
   for sample in getSamples(sound):
     value = getSampleValue(sample)
@@ -25,3 +29,28 @@ def maxSample(sound):
     max_val = max(max_val, getSampleValue(sample))
   return max_val
     
+def isValue(sound, value):
+  max_val = 0
+  for sample in getSamples(sound):
+    if value == getSampleValue(sample):
+      print sample
+
+def maxVolume(sound):
+  largest = maxSample(sound)
+  maxPossibleSampleValue = 32767.0
+  factor=float(maxPossibleSampleValue)/largest
+  for sample in getSamples(sound):
+    value = getSampleValue(sample)
+    setSampleValue(sample, value*factor)
+  writeSoundTo(sound, "C://Developing//School//CST205//maxVolume.wav")
+  
+# 44409:-6663
+def goToEleven(sound):
+  for sample in getSamples(sound):
+    value = getSampleValue(sample)
+    if value > 0:
+      setSampleValue(sample, 32767)
+    elif value < 0:
+      setSampleValue(sample, -32768)
+  writeSoundTo(sound, "C://Developing//School//CST205//goToEleven.wav")
+
