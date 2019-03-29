a3fbb2a72fb05d1bf81f3082652792c630a20701
Roger Dodger <lpaddikt@gmail.com>
2019-03-29 13:36:03 -0700

    Lab9 problem 3

--- CST205/lab9.py
+++ CST205/lab9.py
@@ -1,9 +1,21 @@
 def get_sound():
   return makeSound(pickAFile())
-  
-# start: 30785 - 30790
-# end: 58688
 
+def maxSample(sound):
+  max_val = 0
+  for sample in getSamples(sound):
+    max_val = max(max_val, getSampleValue(sample))
+  return max_val
+  
+def maxVolume(sound):
+  largest = maxSample(sound)
+  maxPossibleSampleValue = 32767.0
+  factor=float(maxPossibleSampleValue)/largest
+  for sample in getSamples(sound):
+    value = getSampleValue(sample)
+    setSampleValue(sample, value*factor)
+  
+# Problem 1
 def clip(source, start, end):
   length = end - start
   index = 0
@@ -14,12 +26,42 @@ def clip(source, start, end):
     index = index + 1
   return empty_sound
     
+# Problem 2
+def copy(source, target, start):
+  source_length = getLength(source)
+  for i in range(source_length-1):
+    value = getSampleValueAt(source, i)
+    setSampleValueAt(target, start, value)
+    start = start + 1
+  return start
+    
+# Problem 3
+def silence(duration):
+  sample_rate = 44100
+  length = int(duration * sample_rate)
+  silence_sound = makeEmptySound(length, sample_rate) 
+  for i in range(length):
+    setSampleValueAt(silence_sound, i, 0)
+  return silence_sound
+
+def sound_collage():
+  sound_list = []
+  num_of_songs = 5
+  length = 0
+  index = 0
+  silence_sound = silence(input("How long would you like the silence between files to be?"))
   
+  for i in range(num_of_songs):
+    sound_list.append(get_sound())
+    
+  for sound in sound_list:
+    length = length + getLength(sound) + getLength(silence_sound)
+  target = makeEmptySound(length, 44100)
+
+  for song in sound_list:
+    index = copy(song, target, index)
+    index = copy(silence_sound,target,index)
+  return target
   
 
 
-def decreaseVolume(sound):
-  for sample in getSamples(sound):
-    value = getSampleValue(sample)
-    setSampleValue(sample, value*0.5)
-  writeSoundTo(sound, "C://Developing//School//CST205//decreaseVol.wav")
