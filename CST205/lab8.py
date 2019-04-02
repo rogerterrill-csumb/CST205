24eec7983a90f13fc217dca6f33bbd0f88c86fbe
Roger Dodger <lpaddikt@gmail.com>
2019-04-01 20:03:54 -0700

    Changed lab 8 to use setMediaPath() instead of hardcoding the path

--- CST205/lab8.py
+++ CST205/lab8.py
@@ -3,25 +3,25 @@ def get_sound():
 
 
 def increaseVolume(sound):
+  setMediaPath()
   for sample in getSamples(sound):
     value = getSampleValue(sample)
     setSampleValue(sample, value*2)
-  writeSoundTo(sound, "C://Developing//School//CST205//increaseVol.wav")
-    
-# Before 10730: 97, 20350: -33
-# After  10730: -62, 20350: -66
+  writeSoundTo(sound, getMediaPath() + 'increaseVol.wav')
 
 def decreaseVolume(sound):
+  setMediaPath()
   for sample in getSamples(sound):
     value = getSampleValue(sample)
     setSampleValue(sample, value*0.5)
-  writeSoundTo(sound, "C://Developing//School//CST205//decreaseVol.wav")
+  writeSoundTo(sound, getMediaPath() + 'decreaseVol.wav')
   
 def changeVolume(sound, factor):
+  setMediaPath()
   for sample in getSamples(sound):
     value = getSampleValue(sample)
     setSampleValue(sample, value*factor)
-  writeSoundTo(sound, "C://Developing//School//CST205//decreaseVol.wav")
+  writeSoundTo(sound, getMediaPath() + 'changeVol.wav')
   
 def maxSample(sound):
   max_val = 0
@@ -30,20 +30,22 @@ def maxSample(sound):
   return max_val
     
 def maxVolume(sound):
+  setMediaPath()
   largest = maxSample(sound)
   maxPossibleSampleValue = 32767.0
   factor=float(maxPossibleSampleValue)/largest
   for sample in getSamples(sound):
     value = getSampleValue(sample)
     setSampleValue(sample, value*factor)
-  writeSoundTo(sound, "C://Developing//School//CST205//maxVolume.wav")
+  writeSoundTo(sound, getMediaPath() + 'maxVol.wav')
   
 def goToEleven(sound):
+  setMediaPath()
   for sample in getSamples(sound):
     value = getSampleValue(sample)
     if value > 0:
       setSampleValue(sample, 32767)
     elif value < 0:
       setSampleValue(sample, -32768)
-  writeSoundTo(sound, "C://Developing//School//CST205//goToEleven.wav")
+  writeSoundTo(sound, getMediaPath() + 'goToEleven.wav')
 
