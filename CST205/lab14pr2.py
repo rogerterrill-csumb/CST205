2877239cb3d204c00e03f3468ee1a4001247dc68
Roger Terrill <rchicasterill@gmail.com>
2019-04-10 15:44:41 -0700

    Lab14pr2 Refactored

--- CST205/lab14pr2.py
+++ CST205/lab14pr2.py
@@ -1,11 +1,10 @@
 def main():
     with open('pressreleases.html', 'r') as f:
+        print("**** U.S. Census Bureau News! ****")
         read_data = list(f)
-        # read_data = str(f.readlines())
         for line in range(len(read_data)):
             if '<p class="uscb-margin-TB-02 uscb-title-3">' in read_data[line]:
                 print(read_data[line+1].strip())
-                # print('FOUND IT~!!!!!!!!!!~@!@#')
 
 
 if __name__ == '__main__':
